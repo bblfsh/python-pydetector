ca86c5c93850222ce2939f6248ba5ffb8d5f316a
Juanjo Alvarez <juanjux@gmail.com>
2017-02-18 00:02:00 +0100

    beautified code, added more tests

--- pydetector/regexp_checks.py
+++ pydetector/regexp_checks.py
@@ -8,7 +8,7 @@ __all__ = ['check_syntax_regex', 'check_modules_regex', 'check_modulesymbols_reg
 
 LINESTART    = r"(^|;)\s*"
 WHITEORSEP   = r"(^|\s|;|:|,|=)+"
-PARENTH_ARGS = r"\s*\(.*\)(\s*|$)"
+PARENTH_ARGS = r"\s*\(.*\)"
 
 PY3OTHER_REGEXP = [
     (re.compile(WHITEORSEP + r"raise\s+.*\s+from\s+None", re.MULTILINE), 100),
@@ -16,21 +16,21 @@ PY3OTHER_REGEXP = [
 ]
 
 PY2OTHER_REGEXP = [
-    (re.compile(WHITEORSEP + r"unicode"    + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"iteritems"  + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"iterkeys"   + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"itervalues" + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"viewkeys"   + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"viewitems"  + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"viewvalues" + PARENTH_ARGS, re.MULTILINE), 25), # XXX
-    (re.compile(WHITEORSEP + r"xrange"     + PARENTH_ARGS, re.MULTILINE), 100), # XXX
-    (re.compile(WHITEORSEP + r"xreadlines" + PARENTH_ARGS, re.MULTILINE), 100), # XXX
-    (re.compile(WHITEORSEP + r"basestring" + PARENTH_ARGS, re.MULTILINE), 100), # XXX
-    (re.compile(WHITEORSEP + r"\.has_key"  + PARENTH_ARGS, re.MULTILINE), 100), # XXX
+    (re.compile(WHITEORSEP + r"unicode"    + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.iterkeys"   + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.iteritems"  + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.itervalues" + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.viewkeys"   + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.viewitems"  + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(r"\w\.viewvalues" + PARENTH_ARGS, re.MULTILINE), 25),
+    (re.compile(WHITEORSEP + r"xrange"     + PARENTH_ARGS, re.MULTILINE), 100),
+    (re.compile(WHITEORSEP + r"xreadlines" + PARENTH_ARGS, re.MULTILINE), 100),
+    (re.compile(WHITEORSEP + r"raw_input"  + PARENTH_ARGS, re.MULTILINE), 100),
+    (re.compile(WHITEORSEP + r"basestring\s*:*[^\(]", re.MULTILINE), 100),
     (re.compile(WHITEORSEP + r"print\s+[^\(]", re.MULTILINE), 100),
-    (re.compile(WHITEORSEP + r"__metaclass__\s*=", re.MULTILINE), 100), # XXX
-    (re.compile(WHITEORSEP + r"raise\s+\w+(\.\w+)?,\s*", re.MULTILINE), 100), # XXX
-    (re.compile(WHITEORSEP + r"raw_input\s*"  + PARENTH_ARGS, re.MULTILINE), 100),
+    (re.compile(WHITEORSEP + r"__metaclass__\s*=", re.MULTILINE), 100),
+    (re.compile(WHITEORSEP + r"raise\s+\w+(\.\w+)?,\s*", re.MULTILINE), 100),
+    (re.compile(r"\w\.has_key"  + PARENTH_ARGS, re.MULTILINE), 100),
 ]
 
 
@@ -158,6 +158,7 @@ def check_syntax_regex(code, matches):
             matches.append(("PY3SYNTAX_"+regtuple[0].pattern, m))
 
     for regtuple in PY2OTHER_REGEXP:
+        print(regtuple[0].pattern)
         m = regtuple[0].findall(code)
         if m:
             py2_score += (regtuple[1] * len(m))
--- tests/test_regexps.py
+++ tests/test_regexps.py
@@ -1,28 +1,36 @@
 import unittest
 from textwrap import dedent
 from pydetector.regexp_checks import check_modules_regex, check_syntax_regex # noqa: E402 #F401
-
+import pydetector.regexp_checks as r
+print(r)
 
 
 class RegexpTestCase(unittest.TestCase):
-    # me quedo en has_key
-    def do_regexp_test(self, testdata, checkmethod):
+    """
+    Helper class, will test the datasnippet with
+    (code, py_expected_points, py3_same, num_matches)
+    and check that the result is the expected one in
+    points and number of matches
+    """
+    def do_regexp_test(self, testdata):
         matches = []
-        ret = checkmethod(testdata[0], matches)
-        print(ret)
+        ret = self.check_method(testdata[0], matches)
         self.assertEqual(ret[0], testdata[1])
         self.assertEqual(ret[1], testdata[2])
         self.assertEqual(len(matches), testdata[3])
 
 
 class TestSyntaxRegex(RegexpTestCase):
+    def setUp(self):
+        self.check_method = check_syntax_regex
+
     def test_exceptions_nonlocal(self):
         code = (dedent("""
             if something:
                 nonlocal var
                 raise SomeException() from None
             """), 0, 200, 2)
-        self.do_regexp_test(code, check_syntax_regex)
+        self.do_regexp_test(code)
 
     def test_raise_except(self):
         code = (dedent("""
@@ -30,21 +38,87 @@ class TestSyntaxRegex(RegexpTestCase):
                 someargo.thing()
                 raise SomeException, stuff
             except SomeException, e
-            """), 200, 0, 2)
-        self.do_regexp_test(code, check_syntax_regex)
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
 
     def test_old_print(self):
         code = (dedent("""
             print "im old"
             print("im not so old")
             """), 100, 0, 1)
-        self.do_regexp_test(code, check_syntax_regex)
+        self.do_regexp_test(code)
 
     def test_rawinput(self):
         code = (dedent("""
             var = raw_input("tell me something:")
             """), 100, 0,  1)
-        self.do_regexp_test(code, check_syntax_regex)
+        self.do_regexp_test(code)
+
+    def test_unicode(self):
+        code = (dedent("""
+            someunicode = unicode(somevar);other()
+            unicode()
+            someunicode()
+            unicode but not method
+            """), 50, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_iteritems(self):
+        code = (dedent("""
+            for item in somedict.iteritems():
+                foo(item)
+            """), 25, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_iterkeys(self):
+        code = (dedent("""
+            for item in somedict.iterkeys():
+                foo(item)
+            """), 25, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_itervalues(self):
+        code = (dedent("""
+            for item in somedict.itervalues():
+                foo(item)
+            """), 25, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_xrange(self):
+        code = (dedent("""
+            for item in xrange(3):
+                foo(item)
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_xreadlines(self):
+        code = (dedent("""
+            for item in xreadlines():
+                foo(item)
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_basestring(self):
+        code = (dedent("""
+            if type(var) == basestring:
+                foo(var)
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_haskey(self):
+        code = (dedent("""
+            d = {1:2}
+            if d.has_key(3):
+              print("fail")
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
+
+    def test_metaclass(self):
+        code = (dedent("""
+            class NotOriginal(object):
+                __metaclass__ = Singleton
+            """), 100, 0, 1)
+        self.do_regexp_test(code)
 
 
 if __name__ == '__main__':
